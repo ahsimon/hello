5a0c0ee8901f83df342dd462b85df51c40500ac6
ahsimon <hesa830505ur8@gmail.com>
2025-01-21 14:31:04 -0600

    fix trigger

---
+++ src/completion.ts
@@ -0,0 +1,146 @@
+import * as vscode from "vscode";
+
+export class InlineCompletionItemProvider implements vscode.InlineCompletionItemProvider {
+    provideInlineCompletionItems(
+        document: vscode.TextDocument,
+        position: vscode.Position,
+        context: vscode.InlineCompletionContext,
+        _token: vscode.CancellationToken
+    ): vscode.ProviderResult<vscode.InlineCompletionItem[] | vscode.InlineCompletionList> {
+        console.log('= context.triggerKind', context.triggerKind)
+        console.log('=InlineCompletionTriggerKind', vscode.InlineCompletionTriggerKind.Automatic)
+        const automaticallyTriggered: boolean = context.triggerKind === vscode.InlineCompletionTriggerKind.Automatic;
+        let results: Array<vscode.InlineCompletionItem> | undefined = [];
+
+        let prefix = _wordBeforePosition(document, position);
+        let suffix = _wordAfterPosition(document, position);
+        if (prefix === undefined && suffix === undefined) {
+            return;
+        }
+        if (prefix === undefined) {
+            prefix = "";
+        }
+        if (suffix === undefined) {
+            suffix = "";
+        } else if (automaticallyTriggered) {
+            return;
+        }
+
+        results = _getCompletionResults(document, position, context, prefix, suffix);
+        if (prefix !== "" && suffix !== "" && results !== undefined && results.length === 0) {
+            results = _getCompletionResults(document, position, context, prefix, "");
+        }
+
+        return results;
+    }
+}
+
+function _getCompletionResults(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    context: vscode.InlineCompletionContext,
+    prefix: string,
+    suffix: string
+): Array<vscode.InlineCompletionItem> | undefined {
+    const automaticallyTriggered: boolean = context.triggerKind === vscode.InlineCompletionTriggerKind.Automatic;
+    const results: Array<vscode.InlineCompletionItem> = [];
+    const previousCompletions = new Map<string, boolean>();
+    const numLinesToSearch = 1000;
+
+    let searchRe;
+    let range: vscode.Range;
+
+    searchRe = new RegExp(`\\b(${prefix}[a-zA-Z0-9_]*)${suffix}\\b`, "g");
+    const prefixLength = prefix.length;
+    range = new vscode.Range(position.translate(0, -1 * prefixLength), position);
+
+    let lineNum = position.line;
+    let colNum = position.character - 1;
+    if (colNum < 0) {
+        colNum = 0;
+    }
+    let targetLineNum = numLinesToSearch >= lineNum + 1 ? 0 : lineNum - numLinesToSearch + 1;
+    while (lineNum > targetLineNum) {
+        let text = document.lineAt(lineNum).text;
+        if (colNum > 0) {
+            text = text.substring(0, colNum);
+        }
+        const matches = [...text.matchAll(searchRe)].reverse();
+        for (const match of matches) {
+            if (match?.index === undefined) {
+                continue;
+            }
+            const candidate = match[1];
+            if (candidate.length === prefixLength) {
+                if (automaticallyTriggered) {
+                    return undefined;
+                }
+                continue;
+            }
+            if (previousCompletions.has(candidate)) {
+                continue;
+            }
+            previousCompletions.set(candidate, true);
+            const completionItem = new vscode.InlineCompletionItem(candidate, range);
+            results.push(completionItem);
+        }
+        lineNum -= 1;
+        colNum = 0;
+    }
+
+    lineNum = position.line;
+    colNum = position.character + 1;
+    const lastLineNum = document.lineCount;
+    targetLineNum = lineNum + numLinesToSearch >= lastLineNum ? lastLineNum : lineNum + numLinesToSearch;
+    while (lineNum < targetLineNum) {
+        let text = document.lineAt(lineNum).text;
+        if (colNum > 0) {
+            text = text.substring(colNum);
+        }
+        const matches = text.matchAll(searchRe);
+        for (const match of matches) {
+            if (match?.index === undefined) {
+                continue;
+            }
+            const candidate = match[1];
+            if (candidate.length === prefixLength) {
+                if (automaticallyTriggered) {
+                    return undefined;
+                }
+                continue;
+            }
+            if (previousCompletions.has(candidate)) {
+                continue;
+            }
+            previousCompletions.set(candidate, true);
+            const completionItem = new vscode.InlineCompletionItem(candidate, range);
+            results.push(completionItem);
+        }
+        lineNum += 1;
+        colNum = 0;
+    }
+
+    return results;
+}
+
+function _wordBeforePosition(document: vscode.TextDocument, position: vscode.Position): string | undefined {
+    let word: string | undefined = undefined;
+    const lineText = document.lineAt(position).text;
+    const leftText = lineText.substring(0, position.character);
+    const match = leftText.match(/([a-zA-Z0-9_]+)$/);
+    if (match !== null) {
+        word = match[1];
+    }
+    return word;
+}
+
+function _wordAfterPosition(document: vscode.TextDocument, position: vscode.Position): string | undefined {
+    let word: string | undefined = undefined;
+    const lineText = document.lineAt(position).text;
+    const rightText = lineText.substring(position.character);
+    const match = rightText.match(/^([a-zA-Z0-9_]+)/);
+    if (match !== null) {
+        word = match[1];
+    }
+    return word;
+}
---
+++ src/extension.ts
@@ -0,0 +1,78 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { InlineCompletionItemProvider } from './completion';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "hello" is now active!');
+
+	const provider:vscode.InlineCompletionItemProvider = {
+			
+		provideInlineCompletionItems(document,position,context,token){
+			console.log('new event!');
+
+			let arr:vscode.InlineCompletionItem[]=[];
+			const item:vscode.InlineCompletionItem = {
+				insertText:'test',
+				range:new vscode.Range(position,position.translate(0,4)),
+				command: {
+                    command: "hello.helloWorld",
+                    title: "Hello World",
+                    arguments: []
+                }
+				
+			}
+			arr.push(item);
+			return arr;
+		}
+	}
+	
+		
+	
+    	context.subscriptions.push(vscode.languages.registerInlineCompletionItemProvider({pattern:'**'},provider));
+
+
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('hello.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from hello!!!');
+	});
+
+	context.subscriptions.push(disposable);
+
+
+
+
+
+
+
+			// Register the provider for plaintext and JavaScript files
+		// const providerDisposable = vscode.languages.registerInlineCompletionItemProvider(
+		// 	{ scheme: 'file' }, // Adjust for your targeted file types
+		// 	new InlineCompletionItemProvider()
+		// );
+
+		// const providerDisposable = vscode.languages.registerInlineCompletionItemProvider(
+		// 	{ pattern: '**' },
+		// 	new InlineCompletionItemProvider()
+		//   )
+	
+		// context.subscriptions.push(providerDisposable);
+
+		
+	
+
+
+}
+	
+// This method is called when your extension is deactivated
+export function deactivate() {}
